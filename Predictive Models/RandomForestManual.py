d524aa647b8033828a304400aa8a1d94d1d14c80
Syed Jawwad Kareem Naqvi <139786195+jawwadkareem@users.noreply.github.com>
2024-07-08 20:04:10 +0500

    Some changes are made and comments are  added for better understanding

--- Predictive Models/RandomForestManual.py
+++ Predictive Models/RandomForestManual.py
@@ -21,25 +21,25 @@ class CustomRandomForestRegressor(BaseEstimator, RegressorMixin):
         np.random.seed(self.random_state)
         for _ in range(self.n_estimators):
             tree = DecisionTreeRegressor(max_features=self.max_features, random_state=np.random.randint(0, 10000))
-            X_sample, y_sample = resample(X, y, random_state=np.random.randint(0, 10000))
+            X_sample, y_sample = resample(X, y, random_state=np.random.randint(0, 10000))#random sampling is done here
             tree.fit(X_sample, y_sample)
             self.trees.append(tree)
         return self
 
     def predict(self, X):
-        tree_preds = np.array([tree.predict(X) for tree in self.trees])
+        tree_preds = np.array([tree.predict(X) for tree in self.trees])#taking the mean of predictions made by all decision trees 
         return np.mean(tree_preds, axis=0)
 
 #Training the model
-# pipeline = Pipeline(steps=[('preprocessor', sae.preprocessor),
-#                            ('regressor', CustomRandomForestRegressor(n_estimators=100, max_features='sqrt',random_state=98))])
-#
-# pipeline.fit(sae.X_train, sae.y_train)
+pipeline = Pipeline(steps=[('preprocessor', sae.preprocessor),
+                           ('regressor', CustomRandomForestRegressor(n_estimators=100, max_features='sqrt',random_state=98))])
 
-# joblib.dump(pipeline, 'car_price_prediction_random_forest_manual1.pkl')
+pipeline.fit(sae.X_train, sae.y_train)
 
-pipeline_loaded = joblib.load('car_price_prediction_random_forest_manual.pkl')
-y_pred = pipeline_loaded.predict(sae.X_test)
+joblib.dump(pipeline, 'car_price_prediction_random_forest_manual1.pkl')
+
+# pipeline_loaded = joblib.load('car_price_prediction_random_forest_manual.pkl')
+# y_pred = pipeline_loaded.predict(sae.X_test)
 
 
 mae = mean_absolute_error(sae.y_test, y_pred)
